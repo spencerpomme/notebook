3c89daf3edbe6f566dd59d87fa1ca2d804764d9e
スペンサー <spencerpomme@live.com>
2019-08-16 18:25:57 +0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
+import os
+import sys
+
+import click
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import redirect, url_for, abort, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', prefix + os.path.join(app.root_path, 'data.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
